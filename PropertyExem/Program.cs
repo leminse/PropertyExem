792c4445586ef652157636bc1e0244694f696d98
minseo <minseo@DESKTOP-NL04HGM>
2025-05-27 14:46:01 +0900

    고전적인 스타일로 Box 클래스 정의

--- PropertyExem/Program.cs
+++ PropertyExem/Program.cs
@@ -10,6 +10,8 @@ namespace PropertyExem
     {
         static void Main(string[] args)
         {
+            Box box = new Box(100, 200);
+            Console.WriteLine(box.Area());
         }
     }
 }
---
+++ PropertyExem/box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyExem
+{
+    class Box
+    {
+        private int width;
+        private int height;
+
+        public Box(int width, int height)
+        {
+            if (width > 0 || height > 0)
+            {
+                this.width = width;
+                this.height = height;
+            }
+            else
+            {
+                Console.WriteLine("너비와 높이는 자연수로 초기화해주세요");
+                //throw new Exception(너비와 높이는 자연수로 초기화해주세요");
+            }
+        }
+
+        public int Area()
+        {
+            return this.width = this.height;
+        }
+    }
+}
